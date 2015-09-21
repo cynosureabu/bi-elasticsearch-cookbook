67708fe8305c2537ffc6f3984e52be7b711aeb34
Chen Wang <chen@boostinsider.com>
2015-09-20 23:55:44 -0700

    install discovery-ec2 plugin

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+# Encoding: utf-8
+name             'elasticsearch_bi'
+maintainer       'Chen Wang'
+maintainer_email 'chen@obostinsider.com'
+license          'Apache 2.0'
+description      'Deploy Es to EC2 vpc'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
+depends 'yum'
+depends 'chef-sugar'
+depends 'elasticsearch'
