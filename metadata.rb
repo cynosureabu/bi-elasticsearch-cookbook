35346b1f452010696e23c795090ba09368802083
Chen Wang <chen@boostinsider.com>
2015-12-23 15:52:53 -0800

    add java dependency

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,5 @@ description      'Deploy Es to EC2 vpc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apt'
-depends 'yum'
-depends 'chef-sugar'
+depends 'java', '>=8.20'
 depends 'elasticsearch', '>= 2.0.0'
